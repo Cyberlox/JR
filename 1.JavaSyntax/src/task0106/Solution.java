0835cc950391a7bad3d6ab4deb441977b67406a6
Rorick <gorochov1990@yandex.com>
2022-05-30 01:03:45 +0300

    Added task 6

---
+++ 1.JavaSyntax/src/task0106/Solution.java
@@ -0,0 +1,18 @@
+package task0106;
+
+public class Solution {
+    /*
+Ну это… С новым годом!
+*/
+    public static void main(String[] args) {
+
+            // System.out.println("Merry New");
+            // System.out.println("weekend");
+            System.out.print("Happy New");
+            // System.out.println("weekend");
+            // System.out.println(s);
+            // System.out.print("Merry New!");
+             System.out.print(" ");
+             System.out.println("Year");
+        }
+    }
