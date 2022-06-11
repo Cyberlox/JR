aeecb50fc9044831837dab63d5e418efa25d8506
Rorick <gorochov1990@yandex.com>
2022-06-12 01:56:42 +0300

    Added task 02-3

---
+++ 1.JavaSyntax/src/task0203/Solution.java
@@ -0,0 +1,7 @@
+package task0203;
+
+public class Solution {
+    public static void main(String []args) {
+        String word = "O Prince", phrase = "O Chief of", line = "many Throned Powers", text = "Seraphim";
+    }
+}
