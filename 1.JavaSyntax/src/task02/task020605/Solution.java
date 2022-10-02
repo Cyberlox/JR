b89ceca5bbe3512d2ba27467f00479abf3a07ec5
Rur <zd1111190@gmail.com>
2022-10-02 23:01:50 +0300

    Local update sunday 1

---
+++ 1.JavaSyntax/src/task02/task020605/Solution.java
@@ -0,0 +1,18 @@
+package task02.task020605;
+
+public class Solution {
+    public static void main(String[] args) {
+        String emptyString = "";
+
+        System.out.println(emptyString);
+        System.out.println("Gommu Gommu no Bazooka!");
+        System.out.println((emptyString + 2 + 2 + "22"));
+        int count1 = emptyString.length();
+        int count2 = "Gommu Gommu no Bazooka!".length();
+        int count3 = (emptyString + 2 + 2 + "22").length();
+        System.out.println(count1);
+        System.out.println(count2);
+        System.out.println(count3);
+
+    }
+}
---
+++ 1.JavaSyntax/src/task020607/Solution.java
@@ -0,0 +1,15 @@
+package task020607;
+
+public class Solution {
+    public static void main(String []args) {
+
+
+        String caps = "if I type in caps";
+        String usa = "сша";
+
+        System.out.println(usa.toUpperCase());
+        System.out.println("Винни Пух".toUpperCase());
+        System.out.println(caps.toUpperCase() + "they know I mean business".toUpperCase());
+
+    }
+}
