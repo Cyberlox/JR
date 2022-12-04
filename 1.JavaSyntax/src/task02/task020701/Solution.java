a1d44a9163b399fc86687bece61984cef5959a32
Rur <zd1111190@gmail.com>
2022-12-04 14:49:34 +0300

    Local update sunday 3 With question

---
+++ 1.JavaSyntax/src/task02/task020701/Solution.java
@@ -0,0 +1,18 @@
+package task02.task020701;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[]args){
+        Scanner console = new Scanner(System.in);
+        String txt = console.nextLine();
+        String txt2 = console.nextLine();
+        String txt3 = console.nextLine();
+
+        System.out.println(txt3);
+        System.out.println(txt2.toUpperCase());
+        System.out.println(txt.toLowerCase());
+
+    }
+}
